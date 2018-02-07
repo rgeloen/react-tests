9fb6b09bc1a4da22b7ffa43dd71c703a31ba25df
rgeloen <rgeloen@hotmail.fr>
2018-02-07 13:26:05 +0100

    styled the different exercices

--- src/partials/scripts/Main.js
+++ src/partials/scripts/Main.js
@@ -10,16 +10,15 @@ class Main extends Component {
     return(
       <BrowserRouter>
         <div>
-          <h1> Hello world </h1>
-          <ul className="header">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/todoList">TodoList</Link></li>
-            <li><Link to="/ipaddressContainer" >IpAdress</Link></li>
-          </ul>
+          <div className="header">
+            <span><Link to="/">Home</Link></span>
+            <span><Link to="/todoList">TodoList</Link></span>
+            <span><Link to="/ipaddressContainer" >IpAdress</Link></span>
+          </div>
           <div className="content">
-           <Route exact path="/" component={Home}></Route>
-           <Route path="/todoList" component={TodoList}></Route>
-           <Route path="/ipaddressContainer" component={IpAdress}></Route>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/todoList" component={TodoList}></Route>
+            <Route path="/ipaddressContainer" component={IpAdress}></Route>
           </div>
         </div>
       </BrowserRouter>
